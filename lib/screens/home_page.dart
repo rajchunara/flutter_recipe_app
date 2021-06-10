8bf46fd52a9b190549863f2f21217e4387aa49ea
rajchunara <rajchunara777@gmail.com>
2021-06-10 09:59:50 -0400

    improved the home screen text field UI

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -47,7 +47,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // double width = MediaQuery.of(context).size.width;
+    double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
       body: (_recipes.length == 0)
@@ -55,76 +55,95 @@ class _HomePageState extends State<HomePage> {
               child: Center(
               child: Text('Loading recipes'),
             ))
-          : SafeArea(
-              child: CustomScrollView(slivers: [
-                SliverAppBar(
-                  collapsedHeight: 80,
-                  backgroundColor: Colors.white,
-                  pinned: true,
-                  expandedHeight: 300.0,
-                  flexibleSpace: FlexibleSpaceBar(
-                      centerTitle: true,
-                      title: Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+          : CustomScrollView(slivers: [
+              SliverAppBar(
+                collapsedHeight: 80,
+                backgroundColor: Colors.white,
+                pinned: true,
+                expandedHeight: 350.0,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                    child: SafeArea(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black12,
+                                offset: Offset(5.0, 5.0),
+                                blurRadius: 8.0),
+                          ],
+                        ),
                         child: TextField(
-                          // style: TextStyle(color: Colors.white),
                           decoration: InputDecoration(
                             isDense: true,
                             contentPadding: EdgeInsets.symmetric(
-                                vertical: 10.0, horizontal: 15.0),
+                                vertical: 7.0, horizontal: 15.0),
                             fillColor: Colors.white,
                             filled: true,
                             enabledBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.all(
                                 Radius.circular(30.0),
                               ),
-                              borderSide: BorderSide(width: 0),
+                              borderSide: BorderSide(style: BorderStyle.none),
                             ),
                             border: OutlineInputBorder(
                               borderRadius: BorderRadius.all(
                                 Radius.circular(30.0),
                               ),
-                              borderSide: BorderSide(width: 0),
+                              borderSide: BorderSide(width: 0.0),
                             ),
                             enabled: true,
-                            hintStyle: TextStyle(fontSize: 15.0),
+                            hintStyle: TextStyle(fontSize: 12.0),
                             hintText: 'Enter a search recipe',
                           ),
                         ),
                       ),
-                      background: Stack(
-                        fit: StackFit.expand,
-                        children: [
-                          Image.asset(
-                            'assets/images/home-page-image.jpg',
-                            fit: BoxFit.cover,
-                          )
-                        ],
-                      ),),
+                    ),
+                  ),
+                  background: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Image.asset(
+                        'assets/images/home-page-image.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                      Positioned(
+                        bottom: 0.0,
+                        left: 0.0,
+                        child: Container(
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                          height: 50,
+                          width: width,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
+              ),
 
-                //Padding widget can not be  used so use SliverList for padding
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                      (BuildContext context, int index) {
-                    return Container(
-                      height: 50.0,
-                    );
-                  }, childCount: 1),
-                ),
+              //Padding widget can not be  used so use SliverList for padding
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                  return Container(
+                    height: 50.0,
+                  );
+                }, childCount: 1),
+              ),
 
-                SliverGrid(
-                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                      maxCrossAxisExtent: 280.0),
-                  delegate: SliverChildBuilderDelegate(
-                      (BuildContext context, int index) {
-                    final recipe = _recipes[index];
-                    return RecipeCard(recipe: recipe);
-                  }, childCount: _recipes.length),
-                )
-              ]),
-            ),
+              SliverGrid(
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 280.0),
+                delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                  final recipe = _recipes[index];
+                  return RecipeCard(recipe: recipe);
+                }, childCount: _recipes.length),
+              )
+            ]),
     );
   }
 }
